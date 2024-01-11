e01b95f113df690b90668544955e2f877ef6401d
Avishek Dutta <79700628+LoGiCGOD@users.noreply.github.com>
2024-01-11 11:53:56 +0530

    Updated main.tf of alb

--- Terraform-VPC/modules/alb/main.tf
+++ Terraform-VPC/modules/alb/main.tf
@@ -13,7 +13,7 @@ resource "aws_lb" "alb" {
 #Listener
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.front_end.arn
+  load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
 
